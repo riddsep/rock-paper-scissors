7d4103fabd39d96d9f41558f3a236cf61270841a
riddsep <ridoseptiawan00@gmail.com>
2024-09-24 23:08:19 +0700

    feat: add rules game

---
+++ script.js
@@ -0,0 +1,67 @@
+const moves = ["rock", "paper", "scissors"];
+const score = {
+  humanScore: 0,
+  computerScore: 0,
+};
+
+function getRandomNumbers() {
+  return Math.floor(Math.random() * moves.length);
+}
+
+function getComputerChoice() {
+  const randomMoves = getRandomNumbers();
+  const computerMoves =
+    randomMoves === 0 ? moves[0] : randomMoves === 1 ? moves[1] : moves[2];
+  return computerMoves;
+}
+
+function getHumanChoice() {
+  const humanMoves = prompt("Rock, Paper or Scissors?");
+  return humanMoves;
+}
+
+function playRound() {
+  const computerMoves = getComputerChoice();
+  const humanMoves = getHumanChoice().toLowerCase();
+
+  let result = "";
+
+  if (computerMoves === "rock") {
+    if (humanMoves === "rock") {
+      result = "tie";
+    } else if (humanMoves === "paper") {
+      result = "win";
+    } else if (humanMoves === "scissors") {
+      result = "lose";
+    }
+  } else if (computerMoves === "paper") {
+    if (humanMoves === "rock") {
+      result = "lose";
+    } else if (humanMoves === "paper") {
+      result = "tie";
+    } else if (humanMoves === "scissors") {
+      result = "win";
+    }
+  } else if (computerMoves === "scissors") {
+    if (humanMoves === "rock") {
+      result = "win";
+    } else if (humanMoves === "paper") {
+      result = "lose";
+    } else if (humanMoves === "scissors") {
+      result = "tie";
+    }
+  }
+
+  switch (result) {
+    case "tie":
+      alert(`Tie You: ${humanMoves} Computer: ${computerMoves}`);
+      break;
+    case "win":
+      alert(`You win!. ${humanMoves} defeat ${computerMoves}`);
+      break;
+    case "lose":
+      alert(`You lose!. ${computerMoves} defeat ${humanMoves}`);
+      break;
+  }
+}
+playRound();
